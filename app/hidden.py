b2a9cda664061003bf54f840ad723091d8b15543
Eddie Rath <eddierath@gmail.com>
2020-02-14 22:57:42 -0500

    twurl update

---
+++ app/hidden.py
@@ -0,0 +1,10 @@
+# Keep this file separate
+
+# https://apps.twitter.com/
+# Create new App and get the four strings
+
+def oauth():
+    return {"consumer_key": "zcdqw7dfQdqlk7egV7UCp929t",
+            "consumer_secret": "mxqC6LcDIFXysbsWF6HOW9RF4q4VhMxkfwSlQM0y0ISUlqqhg1",
+            "token_key": "2721810502-xnSARC1OCgfWqSVQOQC2VfcuhXQGbGnFaA1eNpI",
+            "token_secret": "E7XVmIY66Lr8VEputsvMCgY2fyJrCDBrgCVP1UmYiexFg"}
